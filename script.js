4eac4c8c00cab84dee017a724a348e34e74b409a
root <root@ip-172-31-8-38.eu-west-3.compute.internal>
2023-08-20 06:26:20 +0000

    login file jsscriptv1

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("login-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Here you would typically send the login data to a server for authentication
+    // and handle the response accordingly.
+});
